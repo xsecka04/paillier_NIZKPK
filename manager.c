733adf5cbbcc22691c8b42f6181dc4a5de9df0d6
Martin Sečkar <m.seckar@hotmail.com>
2022-01-28 02:04:50 +0000

    reworked printf

--- manager.c
+++ manager.c
@@ -2,27 +2,27 @@
 #include <gmp.h>
 #include "paillier.h"
 
-void generate_keys(int *buff) {
-    paillier_pubkey_t* pub;
-    paillier_prvkey_t* priv;
-    int len = 1000;
-    paillier_keygen(len, &pub, &priv, paillier_get_rand_devurandom);
-    buff[0] = priv->lambda;
-    buff[1] = pub->n;
-}
 
 int main() {
+
+    //Generating keypairs
     paillier_pubkey_t* pub;
     paillier_prvkey_t* priv;
-    int len = 1000;
+    int len = 100;
     paillier_keygen(len, &pub, &priv, paillier_get_rand_devurandom);
-    printf("Private key (lambda): %d\n", priv->lambda);
-    printf("Public key (n): %d\n", pub->n);
-    char *message = "aasdaasdasd";
+    gmp_printf ("Private key (lambda): %Zd\n", priv->lambda);
+    gmp_printf ("Public key (n): %Zd\n", pub->n);
+
+    char *message = "BigSecret";
+
+    //Encryption of the message
     paillier_ciphertext_t *enc = paillier_enc(0, pub, paillier_plaintext_from_str(message), paillier_get_rand_devurandom);
-    printf("Ciphertext: %d\n", enc->c);
+    gmp_printf ("Ciphertext: %Zd\n", enc->c);
+
+    //Decryption of the ciphertext
     paillier_plaintext_t *dec = paillier_dec(0, pub, priv, enc);
     char *m = paillier_plaintext_to_str(dec);
     printf("Plaintext: %s\n", m);
+
     return 0;
 }
